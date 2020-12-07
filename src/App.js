f61be59a5d3238aac7503e40800f550293ff9c1e
Adama <adama.sonko6978@gmail.com>
2020-12-07 16:49:14 +0100

    import project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootswatch/dist/lux/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App () {
+  return (
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <Main />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/home/CompoCocktail.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Image from '../../images/cocktail.png';
+import './Home.css';
+import { Link } from 'react-router-dom';
+
+const CompoCocktail = (props) => {
+  return (
+    <>
+      <div className='section2 row'>
+        <div className='col-md-12 '>
+          <div className='panel panel-default text-center'>
+            <div className='panel-heading'>
+              <h4> {props.firstPanelh4}</h4>
+            </div>
+            <div className='col-md-12 '>
+              <img className='imageIngredient' src={Image} alt='' />
+            </div>
+            <div className='panel-body'>
+              <p> {props.firstPanelParagraphe}</p>
+            </div>
+            <div className='panel-footer'>
+              <Link to='/find-your-cocktail'><button className='btn btn-lg'>Chose my ingredients</button></Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CompoCocktail;
